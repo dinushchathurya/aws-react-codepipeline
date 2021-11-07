99c7132c15f0cc89c6c8b5a446fd9b342cd4b6ef
dinushchathurya <dinushchathurya21@gmail.com>
2021-11-07 10:25:07 +0530

    fix: Change App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AWS React CodePipeline Demo
+          AWS React
         </p>
       </header>
     </div>
