8f87395807fef9768e6187fa38dad8085335c5bc
dinushchathurya <dinushchathurya21@gmail.com>
2021-11-07 10:20:37 +0530

    fix: Configure buildspec.yml script

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AWS React CodePipeline
+          AWS React CodePipeline Demo
         </p>
       </header>
     </div>
